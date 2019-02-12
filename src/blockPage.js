75531c58d1842f6af84e20e982022925639dde05
Filip Defar <filip.defar@toptal.com>
2019-02-12 12:11:34 +0100

    Extract SlowButton to a Vue.js sfc

--- src/blockPage.js
+++ src/blockPage.js
@@ -1,53 +1,21 @@
 import Vue from "vue/dist/vue.js";
 import "./blockPage.css";
 import quote from "./components/quote.vue";
-const unlockDuration = 1000 * 10; // 10 seconds
-const overrideDuration = 1000 * 60 * 10; // 10 minutes
+import SlowButton from "./components/SlowButton.vue";
 
-// Count from 0 to 100%
-const countTo = 100;
-const countStepDelay = unlockDuration / countTo;
+const overrideDuration = 1000 * 60 * 10; // 10 minutes
 
 new Vue({
   el: "#app",
-  data: { counter: 0, timer: null },
-  computed: {
-    buttonStyleObject: function() {
-      return {
-        background: `linear-gradient(to top, #3C6F9466 ${this.counter}%, ${
-          this.counter
-        }%, #f0f0f0)`
-      };
-    },
-    unlocking: function() {
-      return this.counter > 0;
-    }
-  },
   methods: {
     override: function() {
       const override = new Date(Date.now() + overrideDuration);
       chrome.storage.sync.set({ override });
       window.history.back();
-    },
-    startCountdown: function() {
-      this.count();
-    },
-    stopCountdown: function() {
-      clearTimeout(this.timer);
-      this.counter = 0;
-    },
-    count: function() {
-      this.timer = window.setTimeout(() => {
-        this.counter += 1;
-        if (this.counter >= countTo) {
-          this.override();
-        } else {
-          this.count();
-        }
-      }, countStepDelay);
     }
   },
   components: {
-    quote
+    quote,
+    SlowButton
   }
 });
---
+++ src/components/SlowButton.vue
@@ -0,0 +1,85 @@
+<template>
+  <a
+    v-bind:style="buttonStyleObject"
+    @mousedown="startCountdown"
+    @mouseup="stopCountdown"
+    @mouseout="stopCountdown"
+  >
+    <span v-if="unlocking">{{ counter }}%</span>
+    <span v-else>Hold to unlock</span>
+  </a>
+</template>
+
+<script>
+const unlockDuration = 1000 * 10; // 10 seconds
+const numberOfSteps = 100;
+const countStepDelay = unlockDuration / numberOfSteps;
+
+export default {
+  props: {
+    onUnlocked: Function
+  },
+  data: function() {
+    return { counter: 0, timeoutInstance: null };
+  },
+  computed: {
+    buttonStyleObject: function() {
+      return {
+        background: `linear-gradient(to top, #3C6F9466 ${this.counter}%, ${
+          this.counter
+        }%, #f0f0f0)`
+      };
+    },
+    unlocking: function() {
+      return this.counter > 0;
+    }
+  },
+  methods: {
+    startCountdown: function() {
+      this.count();
+    },
+    stopCountdown: function() {
+      clearTimeout(this.timeoutInstance);
+      this.counter = 0;
+    },
+    count: function() {
+      this.timeoutInstance = window.setTimeout(() => {
+        this.counter += 1;
+        if (this.counter >= numberOfSteps) {
+          this.$emit("unlocked");
+        } else {
+          this.count();
+        }
+      }, countStepDelay);
+    }
+  }
+};
+</script>
+
+<style scoped>
+a,
+a:link,
+a:visited,
+a:focus,
+a:hover,
+a:active {
+  color: #999;
+  font-size: 0.7rem;
+  text-decoration: none;
+  cursor: pointer;
+  width: 7rem;
+  height: 7rem;
+  display: block;
+  border-radius: 50%;
+  border: 4px solid #ccc;
+  text-align: center;
+  line-height: 7rem;
+  margin: 2rem 0;
+}
+
+a span {
+  pointer-events: none;
+  user-select: none;
+  -moz-user-select: none;
+}
+</style>
