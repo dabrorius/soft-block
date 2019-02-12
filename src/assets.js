69619cf9e18a2fd9f5f73108718c4ee410c215a7
Filip Defar <filip.defar@toptal.com>
2019-02-12 15:19:54 +0100

    Clean up assets
    - move all assets under assets folder
    - update icons and logo

--- src/assets.js
+++ src/assets.js
@@ -1,7 +1,7 @@
-import "./icons/icon16.png";
-import "./icons/icon24.png";
-import "./icons/icon32.png";
-import "./icons/icon128.png";
-import "./images/meditate.svg";
-import "./images/logo.svg";
 import "./manifest.json";
+import "./assets/icon16.png";
+import "./assets/icon24.png";
+import "./assets/icon32.png";
+import "./assets/icon128.png";
+import "./assets/meditate.svg";
+import "./assets/logo.svg";
